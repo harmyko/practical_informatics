b35fa8f7f5e85510ebf33c6b6ad6c92ab40dbc27
Ugnius Teišerskis <ugnius.teiserskis@mif.stud.vu.lt>
2024-10-25 12:50:18 +0300

    Vartotojo įvesties surinkimas pirminių skaičių paieškos programoje

---
+++ 07-uzduotis/pirminiai-skaiciai.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print("Pirminių skaičių paieškos programa.")
+print("Programa ras pirminius skaičius nurodytame intervale.")
+skaicius_nuo = input("Įveskite intervalo pradžią: ")
+skaicius_iki = input("Įveskite intervalo pabaigą: ")
+print("Pirminių skaičių ieškoma intervale [" + str(skaicius_nuo) + ", " +str(skaicius_iki) + "]")
+
+
