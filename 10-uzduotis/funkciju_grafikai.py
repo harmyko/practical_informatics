c15869f1bd6a3580f9c2a6dfd3aa35dcaf8c4321
Ugnius Teišerskis <ugnius.teiserskis@mif.stud.vu.lt>
2024-11-21 21:11:54 +0200

    Braižome du papildomus grafikus, pridedame legendą

--- 10-uzduotis/funkciju_grafikai.py
+++ 10-uzduotis/funkciju_grafikai.py
@@ -10,7 +10,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 X = np.linspace(-5, 5, 50001)
+
 F1 = 1/(np.cbrt(X))
-plt.plot(X, F1, linestyle="-", label=r"$f_1(x)=\frac{1}{sqrt[3]{x}}$")
+F2 = 1/X
+F3 = 1/(X ** 2)
+
+plt.plot(X, F1, linestyle="-", label=r"$f_1(x)=\frac{1}{\sqrt[3]{x}}$")
+plt.plot(X, F2, linestyle="--", label=r"$f_2(x)=\frac{1}{x}$")
+plt.plot(X, F3, linestyle=":", label=r"$f_3(x)=\frac{1}{x^2}$")
+
+plt.legend()
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Hiperbolės")
+plt.xlim(-5, 5)
+plt.ylim(-5, 5)
 plt.show()
 plt.close()
